7a3860b82a9e349cb67be7496d9a2594a1ee1ffd
Qwerasd <qwerasd205@users.noreply.github.com>
2017-05-08 19:56:33 -0400

    Improved some functionality

--- noBetterDocs.js
+++ noBetterDocs.js
@@ -1,9 +1,9 @@
 var ref = document.referrer;
 
-if (ref.match(/betterdocs\.net/gi) | ref.match(/goo\.gl/gi) | ref.match(/bit\.ly/gi) | ref.match(/adf\.ly/gi) | ref.match(/sir\.bz/gi) | ref.match(/db\.tt/gi) | ref.match(/qr\.ae/gi) | ref.match(/bitly\.com/gi) | ref.match(/adcrun\.ch/gi) | ref.match(/tr\.im/gi)) {
+if (/betterdocs\.net/gi.test(ref) | /goo\.gl/gi.test(ref) | /bit\.ly/gi.test(ref) | /adf\.ly/gi.test(ref) | /sir\.bz/gi.test(ref) | /db\.tt/gi.test(ref) | /qr\.ae/gi.test(ref) | /bitly\.com/gi.test(ref) | /adcrun\.ch/gi.test(ref) | /tr\.im/gi.test(ref)) {
 	window.location="betterdocs.html";
 }
 
-if (ref.match(/./gi) & !ref.match(/^https?:\/\/qwerasd205.github.io/gi)) {
+if (/./gi.test(ref) & !/^https?:\/\/qwerasd205.github.io/gi.test(ref)) {
     window.location="betterdocs.html";
 }
