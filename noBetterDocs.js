a6930893e359e0a3211b8033c504242027a857bf
Qwerasd <qwerasd205@users.noreply.github.com>
2017-05-08 20:09:58 -0400

    Reduced size

--- noBetterDocs.js
+++ noBetterDocs.js
@@ -1,9 +1,5 @@
 var ref = document.referrer;
 
-if (/betterdocs\.net/gi.test(ref) | /goo\.gl/gi.test(ref) | /bit\.ly/gi.test(ref) | /adf\.ly/gi.test(ref) | /sir\.bz/gi.test(ref) | /db\.tt/gi.test(ref) | /qr\.ae/gi.test(ref) | /bitly\.com/gi.test(ref) | /adcrun\.ch/gi.test(ref) | /tr\.im/gi.test(ref)) {
+if (/betterdocs\.net/gi.test(ref) | /goo\.gl/gi.test(ref) | /bit\.ly/gi.test(ref) | /adf\.ly/gi.test(ref) | /./gi.test(ref) | /sir\.bz/gi.test(ref) | /db\.tt/gi.test(ref) | /qr\.ae/gi.test(ref) | /bitly\.com/gi.test(ref) | /adcrun\.ch/gi.test(ref) | /tr\.im/gi.test(ref) & !/^https?:\/\/qwerasd205.github.io/gi.test(ref)) {
 	window.location="betterdocs.html";
-}
-
-if (/./gi.test(ref) & !/^https?:\/\/qwerasd205.github.io/gi.test(ref)) {
-    window.location="betterdocs.html";
 }
