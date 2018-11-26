ce09887736b7d6d3777640fb25c4bc19c6a1acdc
Qwerasd <qwerasd205@users.noreply.github.com>
2018-11-26 14:06:42 -0500

    0.0.2

--- WordNotifications.plugin.js
+++ WordNotifications.plugin.js
@@ -12,7 +12,7 @@ class WordNotifications {
         return "Get notifications when certain words are said.";
     }
     getVersion() {
-        return "0.0.1";
+        return "0.0.2";
     }
     getAuthor() {
         return "Qwerasd";
@@ -21,6 +21,10 @@ class WordNotifications {
         this.getChannelById = BdApi.findModuleByProps('getChannel').getChannel;
         this.getServerById = BdApi.findModuleByProps('getGuild').getGuild;
         this.transitionTo = BdApi.findModuleByProps('transitionTo').transitionTo;
+        this.isMuted = BdApi.findModuleByProps('isGuildOrCategoryOrChannelMuted').isGuildOrCategoryOrChannelMuted.bind(BdApi.findModuleByProps('isGuildOrCategoryOrChannelMuted'));
+        this.isBlocked = BdApi.findModuleByProps('isBlocked').isBlocked;
+        this.getUnreadCount = BdApi.findModuleByProps('getUnreadCount').getUnreadCount;
+        this.userId = BdApi.findModuleByProps('getId').getId();
     }
     start() {
         this.cancelPatch = BdApi.monkeyPatch(BdApi.findModuleByProps("dispatch"), 'dispatch', { after: this.dispatch.bind(this) });
@@ -31,14 +35,23 @@ class WordNotifications {
         BdApi.saveData('WordNotifications', 'words', this.words);
     }
     dispatch(data) {
-        if (!(data.methodArguments[0].type === 'MESSAGE_CREATE'))
+        if (data.methodArguments[0].type !== 'MESSAGE_CREATE')
             return;
         const message = data.methodArguments[0].message;
+        const channel = this.getChannelById(message.channel_id);
+        const server = this.getServerById(message.guild_id);
+        const author = message.author;
+        if (this.isMuted(message.guild_id, message.channel_id))
+            return;
+        if (message.author.id === this.userId)
+            return;
+        if (this.isBlocked(author.id))
+            return;
         let content = message.content;
         let proceed = false;
         this.words.forEach(word => {
             const regex = new RegExp(`\\b${word}\\b`, 'i');
-            const replaced = content.replace(regex, match => `[${match}]`);
+            const replaced = content.replace(regex, match => `->${match}<-`);
             if (replaced !== content) {
                 proceed = true;
                 content = replaced;
@@ -46,16 +59,14 @@ class WordNotifications {
         });
         if (!proceed)
             return;
-        const channel = this.getChannelById(message.channel_id);
-        const server = this.getServerById(message.guild_id);
-        const author = message.author;
-        const notification = new Notification(`${server.name} #${channel.name} | Word Notifications`, { body: `${author.username}: ${content}` });
+        const notification = new Notification(`${server.name} #${channel.name} (${this.getUnreadCount(channel.id)} unread)`, { body: `${author.username}: ${content}` });
         notification.addEventListener('click', _ => {
             this.goToMessage(server.id, channel.id, message.id);
         });
     }
     goToMessage(server, channel, message) {
         require('electron').remote.getCurrentWindow().focus();
+        this.transitionTo(`/channels/${server}/${channel}/${message}`);
         this.transitionTo(`/channels/${server}/${channel}/${message}`);
     }
     getSettingsPanel() {
@@ -71,6 +82,7 @@ class WordNotifications {
         textarea.style.minHeight = '6ch';
         button.addEventListener('click', _ => {
             this.words = textarea.value.split(',').map(e => e.trim());
+            BdApi.saveData('WordNotifications', 'words', this.words);
             document.getElementById('plugin-settings-Word Notifications').previousSibling.click();
         });
         div.appendChild(textarea);
